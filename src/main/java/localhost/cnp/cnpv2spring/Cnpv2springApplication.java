99fe405c0d10b5fd7af7199cf1aac13ee4f636da
HardLOLMaster <hardlolmaster@bk.ru>
2020-03-28 12:08:33 +0500

    smth

--- src/main/java/localhost/cnp/cnpv2spring/Cnpv2springApplication.java
+++ src/main/java/localhost/cnp/cnpv2spring/Cnpv2springApplication.java
@@ -2,10 +2,8 @@ package localhost.cnp.cnpv2spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 public class Cnpv2springApplication {
 
     public static void main(String[] args) {
